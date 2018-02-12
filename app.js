f3778d6c73abc2410c8ba7b53f06ed386ef4e972
Daniel K <dhavrutsky@gmail.com>
2018-02-12 09:42:09 -0500

    Github commit

--- app.js
+++ app.js
@@ -7,9 +7,10 @@
     User = require("./models/user");
     Request = require("./models/request");
     Design = require("./models/design");
-    paypal = require("paypal-rest-sdk");
 
-mongoose.connect("mongodb://test:test@ds231228.mlab.com:31228/simondesigns", {useMongoClient: true});
+//mongoose.connect("mongodb://test:test@ds231228.mlab.com:31228/simondesigns", {useMongoClient: true}); //PRODUCT
+mongoose.connect("mongodb://localhost/simon_designs", {useMongoClient: true}); //LOCAL testing
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(require("express-session")({
@@ -23,15 +24,9 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 app.set("view engine", "ejs");
-paypal.configure({
-  'mode': 'sandbox', //sandbox or live
-  'client_id': 'AdhtaKc9ZETb7mplptbjv-FB9_ypi6u49-tobnB7JtlQPl4f06n43FVhSZAYoaTdq9iq3ggY1B6GCleT',
-  'client_secret': 'EBwYyRwG6mgYeIMln8pFSIcxG1ffbPBJF_kPpbRYT_Ur8Emd-xRPR503c4d7nrnteCsnyH-olsFE9-fD'
-});
 
-//REALLY SHITTY CODE I KNOW I'LL CLEAN LATER
 
-//Design Schema
+//Add new user to admin
 
 // var newUser = new User({username: "simon"});
 // User.register(newUser, "scorpion1", (err, user) => {
@@ -43,7 +38,7 @@ paypal.configure({
 // })
 
 
-
+//Template for creating designs
 // design.create(
 //   {
 //     name: "RandomDesign6",
@@ -64,8 +59,7 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-
-
+//Design ROUTES
 
 app.get("/designs", (req, res) => {
   Design.find({}, function(err, allDesigns){
@@ -75,51 +69,6 @@ app.get("/designs", (req, res) => {
       res.render("designs", {Design: allDesigns});
     }
   });
-});
-
-
-
-app.get("/requests", (req, res) => {
-  res.render("requests");
-
-});
-
-app.get("/success", (req, res) => {
-  res.render("success");
-  res.redirect("/");
-})
-
-app.get("/admin/requests", isLoggedIn, (req, res) => {
-  Request.find({}, function(err, allReq){
-    if (err){
-      console.log(err);
-    } else{
-      res.render("adminPanel", {Request: allReq});
-    }
-  });
-});
-
-function isLoggedIn(req, res, next){
-  if (req.isAuthenticated()){
-    return next();
-  }
-  res.redirect("/admin/login");
-}
-
-app.post("/requests", (req, res) => {
-  var newRequest = {email: req.body.email, type: req.body.type, additional: req.body.additional};
-
-
-  Request.create(newRequest, (err, newRequest) => {
-    if (err){
-      console.log(err);
-    }
-    else{
-      res.redirect("success");
-    }
-
-  });
-
 });
 
 app.get("/designs/:id", (req, res) => {
@@ -132,23 +81,6 @@ app.get("/designs/:id", (req, res) => {
   });
 });
 
-
-
-//AUTH ROUTES
-
-app.get("/admin/login", (req, res) => {
-  res.render("login");
-})
-// app.post('/login', function(req, res, next) {
-//     console.log(req.url);
-//     passport.authenticate('local', function(err, user, info) {
-//         console.log("authenticate");
-//         console.log(err);
-//         console.log(user);
-//         console.log(info);
-//     })(req, res, next);
-// });
-
 app.post("/delete/:id", (req, res) => {
   Request.remove({ _id: req.params.id }, function(err) {
     if (!err) {
@@ -160,15 +92,59 @@ app.post("/delete/:id", (req, res) => {
   });
 });
 
+//Request ROUTES
+
+app.get("/success", (req, res) => {
+  res.render("success");
+})
 
 
+app.get("/requests", (req, res) => {
+  res.render("requests");
+
+});
+
+app.post("/requests", (req, res) => {
+  var newRequest = {email: req.body.email, type: req.body.type, additional: req.body.additional};
+  Request.create(newRequest, (err, newRequest) => {
+    if (err){
+      console.log(err);
+    }
+    else{
+      res.redirect("success");
+    }
+  });
+});
+
+
+
+///AUTH FUNCTION\\\
+
+function isLoggedIn(req, res, next){
+  if (req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/admin/login");
+}
+
+
+//AUTH ROUTES
+
+app.get("/admin/login", (req, res) => {
+  res.render("login");
+})
+
+app.get("/admin/requests", isLoggedIn, (req, res) => {
+  Request.find({}, function(err, allReq){
+    if (err){
+      console.log(err);
+    } else{
+      res.render("adminPanel", {Request: allReq});
+    }
+  });
+});
 
 app.post('/login', (req, res) => passport.authenticate('local', { successRedirect: '/admin/requests', failureRedirect: '/admin/login', })(req, res));
-
-
-
-
-
 
 
 var port = process.env.PORT || 8080;
